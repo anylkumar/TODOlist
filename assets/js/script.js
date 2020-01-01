72e9cca788b90212cefe719b5eb3fe847c232526
ANIL KUMAR YADAV <anylkumary@gmail.com>
2020-01-01 19:06:28 +0530

    To do List
    To do list

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed") 
+})
+
+$("ul").on("click", "span",function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
+
+
+$("input[type = text]").keypress(function(event){
+ if(event.which === 13){
+    var inputText = $(this).val();
+    $("ul").append("<li><span><i class='fas fa-trash'></i></span> " + inputText + "</li>");
+    $(this).val("");
+    
+  }
+})
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
+
